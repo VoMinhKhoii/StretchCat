4d60fdfee5f8f95cd18c455f20e5e8cc73fb2021
Vo Minh Khoi <minhkhoitdn@gmail.com>
2026-06-12 17:32:42 +0700

    Update Homebrew cask to v1.2.0 (#14)

--- Casks/stretch-cat.rb
+++ Casks/stretch-cat.rb
@@ -1,8 +1,8 @@
 cask "stretch-cat" do
-  version "1.1.0"
-  sha256 "2a4556c4b3b8b3b1274b3e46bf540b6eda15d0ae7063468e8f9b4632c37c61c7"
+  version "1.2.0"
+  sha256 "a83a72012726135927ad0b8eec348d00b9e682c5715c914dab65768a17d3058f"
 
-  url "https://github.com/VoMinhKhoii/StretchCat/releases/download/v1.1.0/StretchCat.dmg"
+  url "https://github.com/VoMinhKhoii/StretchCat/releases/download/v1.2.0/StretchCat.dmg"
   name "Stretch Cat"
   desc "Menu-bar reminder to stand up and stretch every 2 hours, with an animated cat"
   homepage "https://github.com/VoMinhKhoii/StretchCat"
